229f3f9737ad468e1d22c6035dbfc871c4486f99
GennadyEnichek <gennady.enichek@gmail.com>
2023-10-11 20:54:00 +0300

    terraform refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "public_sec_group" {
   }
 
   ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
